6b0ffe7fe3252d192bb315c91283f2c39394b7e8
alexandre <alexandrec658@gmail.com>
2025-03-07 17:24:21 -0300

    refactor: add cofig cors

--- infra/src/main/java/com/alexandre/controle/gastos/infra/rest/login/LoginAPI.java
+++ infra/src/main/java/com/alexandre/controle/gastos/infra/rest/login/LoginAPI.java
@@ -11,11 +11,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping(value = "auth")
+@CrossOrigin(origins = "*")
 @Tag(name = "Authentication", description = "Login")
 public interface LoginAPI {
 
